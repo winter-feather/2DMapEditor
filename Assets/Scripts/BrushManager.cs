2db402e92cac2df0452e527ebff431b827d69fd1
lisuo01 <lisuo01@58.com>
2022-02-07 10:00:49 +0800

    补交笔刷管理

---
+++ Assets/Scripts/BrushManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushManager : SingleMonoManager<BrushManager>
+{
+    public Sprite[] planets;
+    public Dictionary<string, Sprite[]> brush;
+    public List<Texture2D>  gourndTextures;
+    private void Awake()
+    {
+        //brush = new Dictionary<string, Sprite[]>();
+        //brush["NumBrush"] = Resources.LoadAll<Sprite>(@"Brush\NumBrush"); //Resources.Load<Sprite>(@"\NumBrush");
+        //brush["ConerBrush"] = Resources.LoadAll<Sprite>(@"Brush\ConerBrush"); //Resources.Load<Sprite>(@"\NumBrush");
+        gourndTextures = new List<Texture2D>();
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\A"));
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\B"));
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\C"));
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\D"));
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\E"));
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\F"));
+        gourndTextures.Add(Resources.Load<Texture2D>(@"Brush\EdgeConerGround\G"));
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadBrush() { 
+    
+    }
+}
---
+++ Assets/TextureConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextureConverter : MonoBehaviour
+{
+    public RawImage show;
+    // Start is called before the first frame update
+    void Start()
+    {
+        show.texture = CopyToTargetTexture(16, 16);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Texture2D sourceTexture;
+    Texture2D targetTexture;
+
+    public Texture2D CopyToTargetTexture(int sizeX,int sizeY) {
+        int xCount = sourceTexture.width / sizeX;
+        int yCount = sourceTexture.height / sizeY;
+        int targetWidth = xCount * yCount * sizeX, targetHeight = sizeY;
+        targetTexture = new Texture2D(targetWidth, targetHeight);
+        targetTexture.filterMode = FilterMode.Bilinear;
+        int count = 0;
+        for (int i = 0; i < xCount; i++)
+        {
+            for (int j = 0; j < yCount; j++)
+            {
+                targetTexture.SetPixels(count++ * sizeX, 0, sizeX, sizeY, sourceTexture.GetPixels(i * sizeX, j * sizeY, sizeX, sizeY));
+            }
+        }
+        targetTexture.Apply();
+        return targetTexture;
+    }
+}
